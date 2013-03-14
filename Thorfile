37a29afc496ef4cd18c170665d8d69f9e811fa1a
JD Harrington <jd@jdharrington.net>
2013-03-14 17:32:24 -0400

    Initial commit

---
+++ Thorfile
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+require 'bundler'
+require 'bundler/setup'
+require 'thor/foodcritic'
+require 'thor/scmversion'
+require 'berkshelf/thor'
+
+class Default < Thor
+  desc "release", "Tag a new version and upload to chef-server"
+  def release
+    puts "Releasing..."
+  end
+end
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "CHANGEME"
+maintainer       "CHANGEME"
+maintainer_email "CHANGEME"
+license          "All rights reserved"
+description      "Installs/Configures CHANGEME"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0.1.0"
+
